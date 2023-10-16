eb0ee92341abce594e2b43ab3dee9312bc6a3423
Daniel Nikolov <daniel1ni@yahoo.com>
2023-10-16 12:46:19 +0300

    Calculation of circle, rectangle, triangle

---
+++ shapes.py
@@ -0,0 +1,12 @@
+rectangleLength = float(input("Enter the length of the rectangle: "))
+rectangleWidth = float(input("Enter the width of the rectangle: "))
+rectangleArea = rectangleWidth * rectangleLength
+print(f"The rectangle area with length {rectangleArea} and width {rectangleWidth} is: {rectangleArea}")
+import math
+circleRadius = float(input("Enter the the radius of the circle: "))
+circleArea = round(math.pi * pow(circleRadius, 2),2)
+print(f"The area of a circle with radius {circleRadius} is {circleArea}")
+triangleBase = float(input("Enter the base of the triangle: "))
+triangleHeight = float(input("Enter the height of the triangle: "))
+triangleArea = triangleBase * triangleHeight
+print(f"The triangle area with base {triangleBase} and height {triangleHeight} is: {triangleArea}")
